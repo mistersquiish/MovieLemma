618c57f5b0b75072297a33360b36847e71e975a3
Henry Vuong <henryvuong@Henrys-Air.stx.rr.com>
2019-08-22 14:18:40 -0500

    2nd commit

---
+++ MovieLemma/ViewControllers/LoginViewController.swift
@@ -0,0 +1,107 @@
+//
+//  LoginViewController.swift
+//  MovieLemma
+//
+//  Created by Henry Vuong on 8/22/19.
+//  Copyright © 2019 Henry Vuong. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController, UITextFieldDelegate  {
+    
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButtonOutlet: UIButton!
+    @IBOutlet weak var signupOutlet: UIButton!
+    @IBOutlet weak var formFeedbackLabel: UILabel!
+    // Contraints for animations
+    @IBOutlet weak var TopConstraintLogo: NSLayoutConstraint!
+    @IBOutlet weak var BottomConstraintLogo: NSLayoutConstraint!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // UI settings
+        formFeedbackLabel.alpha = 0
+        emailTextField.layer.masksToBounds = false
+        emailTextField.layer.borderWidth = 0.5
+        emailTextField.layer.cornerRadius = 12
+        emailTextField.clipsToBounds = true
+        passwordTextField.layer.masksToBounds = false
+        passwordTextField.layer.borderWidth = 0.5
+        passwordTextField.layer.cornerRadius = 12
+        passwordTextField.clipsToBounds = true
+        loginButtonOutlet.layer.masksToBounds = false
+        loginButtonOutlet.layer.cornerRadius = 12
+        loginButtonOutlet.clipsToBounds = true
+        
+        // add oberserver methods to allow keyboard to dismiss
+        NotificationCenter.default.addObserver(self, selector: #selector(LoginViewController.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(LoginViewController.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        // dismiss keyboard if view is tapped
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
+        tap.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tap)
+        
+        // UITextFieldDelegate to dismiss keyboard on return and next textfield
+        passwordTextField.delegate = self
+        passwordTextField.returnKeyType = .go
+        emailTextField.delegate = self
+        emailTextField.returnKeyType = .next
+        emailTextField.tag = 0
+        passwordTextField.tag = 1
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginButton(_ sender: Any) {
+        
+        
+    }
+    
+    func updateFormFeedback() {
+
+    }
+    
+    // methods for keyboard
+    @objc func keyboardWillShow(notification: NSNotification) {
+        self.TopConstraintLogo.constant = 15
+        self.BottomConstraintLogo.constant = 15
+        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 3, options: .curveEaseInOut, animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+        
+    }
+    @objc func keyboardWillHide(notification: NSNotification) {
+        self.TopConstraintLogo.constant = 60
+        self.BottomConstraintLogo.constant = 30
+        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 3, options: .curveEaseInOut, animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+        // Try to find next responder
+        if let nextField = textField.superview?.viewWithTag(textField.tag + 1) as? UITextField {
+            nextField.becomeFirstResponder()
+        } else {
+            // Not found, so remove keyboard.
+            passwordTextField.resignFirstResponder()
+            // perform login button function
+            
+        }
+        // Do not add a line break
+        return false
+    }
+    
+    
+    
+}
---
+++ MovieLemma/ViewControllers/SignUpViewController.swift
@@ -0,0 +1,116 @@
+//
+//  SignUpViewController.swift
+//  MovieLemma
+//
+//  Created by Henry Vuong on 8/22/19.
+//  Copyright © 2019 Henry Vuong. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var signupButtonOutlet: UIButton!
+    @IBOutlet weak var formFeedbackLabel: UILabel!
+    @IBOutlet weak var signinButtonOutlet: UIButton!
+    // Contraints for animations
+    @IBOutlet weak var TopConstraintLogo: NSLayoutConstraint!
+    @IBOutlet weak var BottomConstraintLogo: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        // UI settings
+        formFeedbackLabel.alpha = 0
+        emailTextField.layer.masksToBounds = false
+        emailTextField.layer.borderWidth = 0.5
+        emailTextField.layer.cornerRadius = 12
+        emailTextField.clipsToBounds = true
+        passwordTextField.layer.masksToBounds = false
+        passwordTextField.layer.borderWidth = 0.5
+        passwordTextField.layer.cornerRadius = 12
+        passwordTextField.clipsToBounds = true
+        nameTextField.layer.masksToBounds = false
+        nameTextField.layer.borderWidth = 0.5
+        nameTextField.layer.cornerRadius = 12
+        nameTextField.clipsToBounds = true
+        signupButtonOutlet.layer.masksToBounds = false
+        signupButtonOutlet.layer.cornerRadius = 12
+        signupButtonOutlet.clipsToBounds = true
+        
+        // add oberserver methods to allow keyboard to dismiss
+        NotificationCenter.default.addObserver(self, selector: #selector(LoginViewController.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(LoginViewController.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        // dismiss keyboard if view is tapped
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
+        tap.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tap)
+        
+        // UITextFieldDelegate to dismiss keyboard on return
+        passwordTextField.delegate = self
+        passwordTextField.returnKeyType = .go
+        passwordTextField.tag = 2
+        emailTextField.delegate = self
+        emailTextField.returnKeyType = .next
+        emailTextField.tag = 1
+        nameTextField.delegate = self
+        nameTextField.returnKeyType = .next
+        nameTextField.tag = 0
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func signupButton(_ sender: Any) {
+        
+        
+    }
+    
+    func updateFormFeedback() {
+        return
+    }
+    
+    // methods for keyboard
+    @objc func keyboardWillShow(notification: NSNotification) {
+        self.TopConstraintLogo.constant = 15
+        self.BottomConstraintLogo.constant = 15
+        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 3, options: .curveEaseInOut, animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+        
+    }
+    @objc func keyboardWillHide(notification: NSNotification) {
+        self.TopConstraintLogo.constant = 60
+        self.BottomConstraintLogo.constant = 30
+        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 3, options: .curveEaseInOut, animations: {
+            self.view.layoutIfNeeded()
+        }, completion: nil)
+    }
+    
+    // dismiss keyboard when return is hit
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+        // Try to find next responder
+        if let nextField = textField.superview?.viewWithTag(textField.tag + 1) as? UITextField {
+            nextField.becomeFirstResponder()
+        } else {
+            // Not found, so remove keyboard.
+            passwordTextField.resignFirstResponder()
+            // perform signup button function
+            
+        }
+        // Do not add a line break
+        return false
+    }
+    
+}
